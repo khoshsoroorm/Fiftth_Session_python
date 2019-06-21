da98bbfbc8367fd2e2411d6cf7e946db80ca2e0a
khoshsoroorm <khoshsoroorm@gmail.com>
2019-06-21 09:19:55 +0330

    is triangle practice DONE

--- is_triangle.py
+++ is_triangle.py
@@ -1,6 +1,7 @@
 def is_triangle(a, b, c):
-    condition = a < b + c and b < a + c and c < a + b
-    return condition
+
+    return a < b + c and b < a + c and c < a + b
+
 
 
 print(is_triangle(3, 4, 15))
